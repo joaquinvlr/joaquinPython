c42c2406d125fbdee4931fe94945941cf7fee297
joaquinvlr <juaco.agc16@gmail.com>
2026-04-13 10:11:49 -0400

    creando variables

---
+++ prueba.py
@@ -0,0 +1,17 @@
+# creando variables
+
+
+titulo="clima de hoy" #string
+diadelmes=13 #int
+mes=4
+temperatura=22.3 #float
+llueve=False #boolean
+
+print(titulo)
+print("temperatura actual" , temperatura, "grados")
+print(diadelmes, "-", mes)
+
+if llueve==True:
+    print("tiene que llevar paraguas")
+else:
+    print("puede salir desnudo")
