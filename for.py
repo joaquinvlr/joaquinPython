825ac81f44678363ae478549b7fbfd0d7f3ad14c
joaquinvlr <juaco.agc16@gmail.com>
2026-04-15 17:06:04 -0400

    For.py

---
+++ for.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+# num=int(input("ingrese un numero: "))
+
+# for i in range(num):
+#     print("Hola joaquin")
+
+
+
+
+# num=int(input("ingrese un numero: "))
+
+# for i in range(10):
+#     print(num, "x" , i+1 , "=" , num*(i+1))
+
+
+# num=int(input("ingrese la cantidad de notas: "))
+
+# for i in range(num):
+#     print ("ingrese la nota ", i+1, ":")
+#     nota
+
+
+# alums=int(input("ingrese la cantidad de alumnos: "))
+
+
